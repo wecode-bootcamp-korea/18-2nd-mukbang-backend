ed46d7bbfb3346c72ab08ebfa1aa6df3b257e970
taekhyang <xorgid1195@gmail.com>
2021-03-31 21:31:18 +0900

    ADD : added Kakao login auth logic & unittest

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,72 @@
-from django.test import TestCase
+import json
+import requests
 
-# Create your tests here.
+from django.test import TestCase
+from django.test import Client
+from django.http import JsonResponse
+
+from unittest.mock import patch, MagicMock, Mock
+
+from .views import KakaoLoginView
+
+
+class KakaoLoginTest(TestCase):
+    def test_kakaologin_get_success(self):
+        mock_content  = {'id': '12345678', 'properties': {'nickname': 'test_nickname'}}
+
+        mock_response          = requests.Response()
+        mock_response._content = bytes(json.dumps(mock_content), 'utf-8')
+
+        with patch('requests.get', return_value=mock_response):
+            client = Client()
+            params = {'access_token': 'test_access_token'}
+            response = client.get('/user/login/kakao', data=params, content_type='application/json').json()
+
+            self.assertEqual(response.get('nickname'), 'test_nickname')
+            self.assertTrue(response.get('token'), True)
+
+    def test_kakaologin_get_fail_access_token_not_exist(self):
+        client = Client()
+        params = {'access_token': ''}
+        response = client.get('/user/login/kakao', data=params, content_type='application/json').json()
+
+        self.assertEqual(response.get('message'), 'ACCESS_TOKEN_DOES_NOT_EXIST')
+
+    def test_kakaologin_get_fail_with_error_code(self):
+        mock_content  = {'code': -401}
+
+        mock_response          = requests.Response()
+        mock_response._content = bytes(json.dumps(mock_content), 'utf-8')
+
+        with patch('requests.get', return_value=mock_response):
+            client = Client()
+            params = {'access_token': 'test_access_token'}
+            response = client.get('/user/login/kakao', data=params, content_type='application/json').json()
+
+        self.assertIn('code -401', response.get('message'))
+
+    def test_kakaologin_get_fail_with_invalid_token(self):
+        mock_content  = {'id': ''}
+
+        mock_response          = requests.Response()
+        mock_response._content = bytes(json.dumps(mock_content), 'utf-8')
+
+        with patch('requests.get', return_value=mock_response):
+            client = Client()
+            params = {'access_token': 'test_access_token'}
+            response = client.get('/user/login/kakao', data=params, content_type='application/json').json()
+
+        self.assertEqual(response.get('message'), 'INVALID_ACCESS_TOKEN')
+
+    def test_kakaologin_get_fail_with_key_error(self):
+        mock_content  = {'id': '12345678'}
+
+        mock_response          = requests.Response()
+        mock_response._content = bytes(json.dumps(mock_content), 'utf-8')
+
+        with patch('requests.get', return_value=mock_response):
+            client = Client()
+            params = {'access_token': 'test_access_token'}
+            response = client.get('/user/login/kakao', data=params, content_type='application/json').json()
+
+        self.assertEqual(response.get('message'), 'KEY_ERROR')
--- user/views.py
+++ user/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+import jwt
+import requests
+import uuid
 
-# Create your views here.
+from django.shortcuts import redirect
+from django.views     import View
+from django.http      import JsonResponse
+
+from .models import User
+
+from my_settings import SECRET_KEY, HASHING_ALGORITHM, KAKAO_RESTAPI_KEY
+
+
+KAKAO_USERINFO_REQUEST_URL = 'https://kapi.kakao.com/v2/user/me'
+
+
+class KakaoLoginView(View):
+    def get(self, request):
+        try:
+            access_token = request.GET.get('access_token')
+            if not access_token:
+                return JsonResponse({'message': 'ACCESS_TOKEN_DOES_NOT_EXIST'}, status=400)
+
+            headers  = {'Authorization': 'Bearer {}'.format(access_token)}
+            response = requests.get(KAKAO_USERINFO_REQUEST_URL, headers=headers).json()
+            
+            res_code = response.get('code')
+            if res_code == -401:
+                return JsonResponse({'message': 'code -401 : {}'.format(response.get('msg'))}, status=400)
+
+            kakao_user_id = response.get('id')
+
+            if kakao_user_id:
+                kakao_user_nickname = response['properties']['nickname']
+
+                user, _   = User.objects.get_or_create(kakao_id=kakao_user_id)
+                jwt_token = jwt.encode({'user_id': user.id}, SECRET_KEY, algorithm=HASHING_ALGORITHM)
+
+                user_info = {'token': jwt_token, 'nickname': kakao_user_nickname}
+                return JsonResponse(user_info, status=200)
+            else:
+                return JsonResponse({'message': 'INVALID_ACCESS_TOKEN'}, status=400)
+
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
